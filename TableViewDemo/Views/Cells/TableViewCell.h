0489e652442f04f763cf4d536b7d58f5caf2b5b2
Htet Moe Phyu <hmp.ucsm@gmail.com>
2021-11-06 15:35:00 +0630

    initial commit

---
+++ TableViewDemo/Views/Cells/TableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.h
+//  TableViewDemo
+//
+//  Created by Htet Moe Phyu on 06/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
